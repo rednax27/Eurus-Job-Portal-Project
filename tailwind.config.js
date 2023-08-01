2d6e4ed06c8e4e9014bb5a59bf3f78645c3bbc4e
rednax27 <xandeeyu@gmail.com>
2023-08-01 18:49:03 +0800

    added header and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/**/*.ejs"],
+  content: ['./views/**/*.ejs'],
   theme: {
     extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+      },
       colors: {
-        "mountain-meadow": {
-          DEFAULT: "#10B981",
-          50: "#D7FCEF",
-          100: "#C4FAE8",
-          200: "#9EF7D9",
-          300: "#79F3CB",
-          400: "#53F0BC",
-          500: "#2EEDAE",
-          600: "#13DF9B",
-          700: "#10B981",
-          800: "#0C855D",
-          900: "#075239",
-          950: "#053827",
+        'mountain-meadow': {
+          DEFAULT: '#10B981',
+          50: '#D7FCEF',
+          100: '#C4FAE8',
+          200: '#9EF7D9',
+          300: '#79F3CB',
+          400: '#53F0BC',
+          500: '#2EEDAE',
+          600: '#13DF9B',
+          700: '#10B981',
+          800: '#0C855D',
+          900: '#075239',
+          950: '#053827',
         },
       },
     },
