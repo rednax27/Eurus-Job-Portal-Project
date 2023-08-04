d8a963400ecf5fe9c85052f3f1099a16d6269c45
rednax27 <xandeeyu@gmail.com>
2023-08-04 20:22:35 +0800

    fixed link to signup_freelancer page

--- index.js
+++ index.js
@@ -1,40 +1,46 @@
-const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
-const path = require("path");
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
 
 const app = express();
 
 app.use(expressLayouts);
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.set("view engine", "ejs");
-app.set("layout", "./layouts/master");
+app.set('view engine', 'ejs');
+app.set('layout', './layouts/master');
 
-app.get("/", function (req, res) {
-  res.render("pages/index");
+app.get('/', function (req, res) {
+  res.render('pages/index');
 });
 
-app.get("/about", function (req, res) {
-  res.render("pages/about");
+app.get('/about', function (req, res) {
+  res.render('pages/about');
 });
 
-app.get("/login_freelancer", function (req, res) {
-  res.render("pages/login_freelancer", {
-    layout: "./layouts/authentication",
+app.get('/login_freelancer', function (req, res) {
+  res.render('pages/login_freelancer', {
+    layout: './layouts/authentication',
   });
 });
 
-app.get("/login_employer", function (req, res) {
-  res.render("pages/login_employer", {
-    layout: "./layouts/authentication",
+app.get('/login_employer', function (req, res) {
+  res.render('pages/login_employer', {
+    layout: './layouts/authentication',
   });
 });
 
-app.get("/signup_employer", function (req, res) {
-  res.render("pages/signup_employer", {
-    layout: "./layouts/authentication",
+app.get('/signup_employer', function (req, res) {
+  res.render('pages/signup_employer', {
+    layout: './layouts/authentication',
+  });
+});
+
+app.get('/signup_freelancer', function (req, res) {
+  res.render('pages/signup_freelancer', {
+    layout: './layouts/authentication',
   });
 });
 
 app.listen(3000);
-console.log("Server is running");
+console.log('Server is running');
